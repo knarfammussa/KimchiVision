a0d4aff13bdaa26d2c6dbd6b13961f464f77f7e6
Damon Lin <damonlin93410@gmail.com>
2025-06-08 14:09:11 -0700

    loss

--- lstm/loss.py
+++ lstm/loss.py
@@ -65,5 +65,4 @@ class MotionLoss(nn.Module):
         classification_loss = F.cross_entropy(pred_scores, targets)
 
         loss = (self.reg_weight * regression_loss + self.cls_weight * classification_loss)
-
         return loss.mean()
